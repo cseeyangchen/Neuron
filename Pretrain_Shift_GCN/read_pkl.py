db577f2762a01eba665ef0d6cc4d28738ca790f3
Yang Chen <152866119+cseeyangchen@users.noreply.github.com>
2024-11-15 14:43:35 +0800

    Delete Pretrain_Shift_GCN/read_pkl.py

--- Pretrain_Shift_GCN/read_pkl.py
+++
@@ -1,8 +0,0 @@
-import pickle
-path = '/root/autodl-tmp/FGE/Pretrain_Shift_GCN/work_dir/ntu60_ShiftGCN_joint_xview_seen55_unseen5/eval_results/epoch_6_0.1288965715932008.pkl'
-
-f = open(path, 'rb')
-data = pickle.load(f)
-
-print(data)
-print(len(data))
